fb16350a76a3dd05cc7573157f3784db13e5c3ee
CHIAO KUANG HENG <tobby8668@gmail.com>
2025-04-30 20:50:47 +0800

    架構重新修整

--- Assets/Code/Scripts/Character/Controller.cs
+++ Assets/Code/Scripts/Character/Controller.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Controller : MonoBehaviour
+public class Controller
 {
     private CharacterController _characterController;
     private Camera _camera;
     private Vector2 _lookDeltaInput;
     private Vector2 _playerInput;
-    [Header("Sensitivity")]
-    [SerializeField] private float sensitivity;
-    
-    
-    void Start()
+    private float sensitivity = 20;
+    private Transform _playerTransform;
+
+
+    public Controller(CharacterController characterController, Camera camera,Transform playerTransform)
     {
-        _characterController = GetComponentInChildren<CharacterController>();
-        _camera = GetComponentInChildren<Camera>();
+        _characterController = characterController;
+        _camera = camera;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        _playerTransform = playerTransform;
     }
-    void Update()
+    
+    public void LookUpdate()
+    {
+        LookRotate();
+    }
+
+    public void SetLook(Vector2 value)
+    {
+        _lookDeltaInput = value;
+    }
+
+    private void LookRotate()
     {
         float mouseX = _lookDeltaInput.x * sensitivity * Time.deltaTime;
         float mouseY = _lookDeltaInput.y * sensitivity * Time.deltaTime;
@@ -26,13 +37,10 @@ public class Controller : MonoBehaviour
         _playerInput.x += mouseX;
         _playerInput.y -= mouseY;
         
-        transform.localRotation = Quaternion.Euler(0f, _playerInput.x, 0f);
+        _playerTransform.localRotation = Quaternion.Euler(0f, _playerInput.x, 0f);
         
         _camera.transform.localRotation = Quaternion.Euler(_playerInput.y, 0f, 0f);
     }
 
-    public void OnLook(InputValue value) 
-    {
-        _lookDeltaInput = value.Get<Vector2>();
-    }
+
 }
